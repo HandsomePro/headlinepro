ebf6b67cfd0247a25088b1f7ccc670a7713dcc6d
baiyimao <17792657173@163.com>
2021-01-25 21:44:38 +0800

    add pytest

---
+++ scripts/test01.py
@@ -0,0 +1,12 @@
+class TestLogin:
+    def test1(self):
+        print('1>>>')
+        assert True
+
+    def test2(self):
+        print('2>>>')
+        assert 1 > 0
+
+    def test3(self):
+        print('3>>>')
+        assert 'a' in 'abc'
---
+++ scripts/test2.py
@@ -0,0 +1,15 @@
+class TestLogin:
+    # def setup(self):
+    #     # 放置前置代码
+    #     print("setup")
+    # def teardown(self):
+    #     # 退出driver对象
+    #     print("teardown")
+    # def setup_class(self):
+    #     print("setup_class")
+    # def teardown_class(self):
+    #     print("teardown_class")
+    # def test_a(self):
+    #     print("a")
+    def test_b(self):
+        print("b")
